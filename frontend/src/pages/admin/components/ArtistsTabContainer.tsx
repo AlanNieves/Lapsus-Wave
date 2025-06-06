3757de078523d1d36d320897855f25560d89d9c5
Chris Peralta <st3477@utr.edu.mx>
2025-06-06 10:09:59 -0600

    Add Playlist agregado

---
+++ frontend/src/pages/admin/components/ArtistsTabContainer.tsx
@@ -0,0 +1,11 @@
+import ArtistsTable from "./ArtistsTable";
+
+const ArtistsTabContainer = () => {
+  return (
+    <div className="space-y-6">
+      <ArtistsTable />
+    </div>
+  );
+};
+
+export default ArtistsTabContainer;
